105f766e78711f03b8fba867fec5d70d270e4389
Marc Pope <11093841+marcpope@users.noreply.github.com>
2021-10-20 15:16:12 -0400

    Create Podcasts.php

---
+++ classes/Podcasts.php
@@ -0,0 +1,30 @@
+<?php
+	
+class Podcasts extends Database {
+
+	/* getShows from database limit by qty */		
+	public function getRecent($qty) {
+			$sql = "SELECT * from podcasts ORDER BY dt_article DESC LIMIT $qty";
+       
+			$stmt	= $this->connect()->prepare($sql);
+					  $stmt->execute(); 
+			$recentPodcasts  = $stmt->fetchAll();
+		}
+		return $recentPodcasts;
+	}	
+
+	public function getPodcast($podcastid) {			
+										
+			$sql = "select * from podcasts where podcastid=$podcastid";
+			
+			$stmt	= $this->connect()->prepare($sql);
+					  $stmt->execute(); 
+			$podcast = $stmt->fetchAll();	
+				
+		return $podcast[0];
+	}	
+
+		
+}
+
+?>
