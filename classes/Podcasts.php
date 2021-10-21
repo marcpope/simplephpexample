43da774ce73a50663f57e8e2da31e8352e68b9ba
Marc Pope <11093841+marcpope@users.noreply.github.com>
2021-10-21 11:09:48 -0400

    Update Podcasts.php

--- classes/Podcasts.php
+++ classes/Podcasts.php
@@ -4,7 +4,7 @@ class Podcasts extends Database {
 
 	/* getShows from database limit by qty */		
 	public function getRecent($qty) {
-			$sql = "SELECT * from podcasts ORDER BY dt_article DESC LIMIT $qty";
+			$sql = "SELECT * FROM podcasts ORDER BY dt_article DESC LIMIT $qty";
        
 			$stmt	= $this->connect()->prepare($sql);
 					  $stmt->execute(); 
@@ -16,7 +16,7 @@ class Podcasts extends Database {
 	/* get a single podcast */
 	public function getPodcast($podcastid) {			
 										
-			$sql = "select * from podcasts where podcastid=$podcastid";
+			$sql = "SELECT * FROM podcasts WHERE podcastid=$podcastid";
 			
 			$stmt	= $this->connect()->prepare($sql);
 					  $stmt->execute(); 
